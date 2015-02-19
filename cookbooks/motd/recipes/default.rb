4ce117c9196d02124eabf6bb5642b16ccdcf7071
Spenser Clark <saclark@uci.edu>
2015-02-19 22:47:48 +0000

    recipe made to install ntp and make sure it's config is standard

--- cookbooks/motd/recipes/default.rb
+++ cookbooks/motd/recipes/default.rb
@@ -3,9 +3,9 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
-file "/etc/motd" do
+template "/etc/motd" do
 	action :create
-	content "Property of UCI School of Education"
+	source "motd.erb"
 	mode "0644"
 	owner "root"
 	group "root"
