4ba7ed7843a3410095ddd72d8b18704694e26d78
Spenser Clark <saclark@uci.edu>
2015-02-19 22:09:26 +0000

    Initial chef-repo

--- cookbooks/motd/recipes/default.rb
+++ cookbooks/motd/recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+file "/etc/motd" do
+	action :create
+	content "Property of UCI School of Education"
+	mode "0644"
+	owner "root"
+	group "root"
+end
